336c2e150429f61ba5605227c424a673984a8835
thibaut1304 <thibaut.husser@free.fr>
2022-01-01 17:08:46 +0100

    ex01 ok

---
+++ module00/ex01/megaphone.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <locale>
+#include <cctype>
+
+int main(int argc, char **argv)
+{
+    if (argc == 1)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    else
+    {
+        for (int i(1); i < argc; i++)
+        {
+            std::string str = argv[i];
+            for (size_t y(0); y < str.length(); y++)
+                std::cout << (char)std::toupper(str[y]);
+        }
+        std::cout << std::endl;
+    }
+    return (0);
+}
